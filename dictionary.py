2c36a58f473da7964ec8cccc1e0b128cf4f096a1
johntran <tranjohn1211@gmail.com>
2022-10-17 12:13:42 -0700

    order coffee

--- dictionary.py
+++ dictionary.py
@@ -21,5 +21,27 @@ for value in donations.values():
 
 # print(total_donation)
 
-if 'colt' in instructor.values():
-    print (True)
+# if 'colt' in instructor.values():
+#     print (True)
+
+inventory = {'croissant': 19, 'bagel': 4, 'muffin': 8, 'cake': 1} #DON'T CHANGE THIS LINE!
+# Make a copy of inventory and save it to a variable called stock_list
+stock_list = inventory.copy()
+# add the value 18 to stock_list under the key "cookie"
+stock_list['cookie'] = 18
+# remove 'cake' from stock_list
+stock_list.pop('cake')
+
+playlist = {
+    'title': 'patgaonia bus',
+    'author': 'colt steele',
+    'songs': [
+        {'title': 'song1', 'artist': ['blue'], 'duraiton': 2.5},
+        {'title': 'song2', 'artist': ['kitty'], 'duraiton': 5.25},
+        {'title': 'meowmewo', 'artist': ['garfield'], 'duraiton': 2.},
+
+    ]
+}
+
+for sing in playlist['songs']:
+    print(sing['title'])
